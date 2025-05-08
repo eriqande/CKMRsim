1195010380b65d9c4feaa6f7af802a606f39178d
Eric C. Anderson <eric.anderson@noaa.gov>
2025-05-07 21:29:59 -0600

    Added the ability to simulate by missing data patterns

--- R/simulate_missing_data_array.R
+++ R/simulate_missing_data_array.R
@@ -1,4 +1,78 @@
+#### Some Helper Functions First ##########
 
+#' Collect Lower Triangle Pairs by Value
+#'
+#' Given a symmetric square integer matrix `M`, this function extracts all (r, c)
+#' index pairs from the lower triangle (r > c) for each unique value `V*` present
+#' in that region of the matrix. The result is a named list where each name corresponds
+#' to a value `V*`, and each element is a list containing two integer vectors: r and c.
+#'
+#' @param M A square numeric matrix. Assumed to be symmetric with integer-like values.
+#'
+#' @return A named list. Each element corresponds to a unique value in the lower triangle of `M`,
+#'         and contains a list with components r and c, the row and column indices (with r > c)
+#'         where that value occurs.
+#'
+#' @examples
+#' set.seed(1)
+#' M <- matrix(sample(80:180, size = 100^2, replace = TRUE), nrow = 100)
+#' result <- collect_lower_triangle_pairs(M)
+#' str(result[["100"]])
+#'
+collect_lower_triangle_pairs <- function(M) {
+  stopifnot(is.matrix(M))
+  stopifnot(nrow(M) == ncol(M))
+
+  n <- nrow(M)
+
+  # Use which() on the lower triangle to get indices
+  lower_indices <- which(lower.tri(M), arr.ind = TRUE)
+  values <- M[lower_indices]
+
+  # Split the row and column indices by the value in the matrix
+  split_indices <- split(as.data.frame(lower_indices), values)
+
+  # Format the result as requested: list named by V*, each element is a list with $r and $c
+  result <- lapply(split_indices, function(df) list(r = df$row, c = df$col))
+
+  return(result)
+}
+
+
+
+#' make a miss_mask_mat for simulate_Qij()
+#'
+#' This uses the missing data patterns at pairs of individuals
+#' to specify the missing data patterns.
+#' @param MG a matrix of N rows and L columns with a 0 if the data are missing
+#' and a 1 otherwise.
+#' @param L a list with two elements, r and c, each one a vector of the same length.
+#' Each pair of r,c values gives a pair of individuals (rows in MG) whose missing
+#' data patterns are to be used.
+#' @param reps Minimum number of Monte Carlo reps to do.
+#' @return A miss_mask_mat: A logical matrix with L columns and R rows. The (r,c)-th
+#' element is TRUE if the c-th locus should be considered missing in the r-th
+#' simulated sample. Note that the number of rows is the number of Monte Carlo
+#' reps that should be done.
+make_miss_mask_mat <- function(MG, L, reps) {
+  NP <- length(L$r)
+  Recyc <- 1  # assume only only copy of the patterns
+  if(NP < reps) {
+    Recyc <- ceiling(reps / NP)
+  }
+  Mat <- lapply(1:NP, function(i) {
+    !as.logical(MG[L$r[i],] *  MG[L$c[i],])
+  }) %>%
+    do.call(what = rbind, args = .)
+
+  # now, recycle rows as necessary
+  recy <- rep(1:NP, times = Recyc)
+  ret <- Mat[recy,]
+  ret
+}
+
+
+######## END OF HELPER FUNCTIONS #################
 
 #' Simulate Qijs across multiple relevant levels of missing data
 #'
@@ -39,6 +113,12 @@
 #' of pairwise shared genotypes, drawing the pattern of missingness from a very simple
 #' independent random sampling model according to the frequency of missing data in all
 #' loci.
+#' - `pairwise_patterns`: do a separate simulation for each number of shared non-missing
+#' loci observed amongst all pairs.  The simulation will do at least one rep for each
+#' pair within a category of the number of shared non-missing loci, and it will use
+#' the observed patterns of missing loci for all pairs. If the number Np of pairs in a category
+#' is less than `reps`, then the actual number of reps done will be the smallest integer
+#' multiple of Np >= `reps`, and the patterns of missing data will be recycled.
 #' @inheritDotParams simulate_Qij sim_relats calc_relats reps
 #'
 #' @export
@@ -73,8 +153,6 @@ simulate_missing_data_array <- function(
   ) %>%
     count(num_non_miss)
 
-  rm(big)
-
   pairwise_non_miss_counts_plot <- ggplot(pairwise_non_miss_counts, aes(x = num_non_miss, y = n)) +
     geom_col() +
     xlab("Number of loci missing in neither pair member") +
@@ -90,6 +168,13 @@ simulate_missing_data_array <- function(
     geom_col() +
     xlab("Number of non-missing loci") +
     ylab("Number of individuals")
+
+
+  #### Step 1.5*.  Collect the pairs that have the same number of shared loci (if needed)
+  if(simulation_approach == "pairwise_patterns") {
+    LTpairs <- collect_lower_triangle_pairs(big)
+  }
+
 
   #### Step 2. Estimate missing data rate per locus   ####
 
@@ -113,6 +198,75 @@ simulate_missing_data_array <- function(
   if(tabulate_and_exit) {
     return(ret)
   }
+
+
+
+  #### If we are simulating using pairwise patterns, we can just do it here
+  if(simulation_approach == "pairwise_patterns") {
+    # deal with ... stuff
+    dotL <- list(...)
+    bad_params_logi <- !(names(dotL) %in% c("sim_relats", "calc_relats", "reps"))
+    if(sum(bad_params_logi) > 1) {
+      bad_params <- names(dotL)[bad_params_logi]
+      message("Ignoring ... params: ", paste(bad_params, collapse = ", "))
+    }
+    good_params <- dotL[!bad_params_logi]
+
+    # first, we simulate the Qijs unlinked
+    Qijs_unlinked <- parallel::mclapply(
+      LTpairs,  #### Just do first 20 for testing!
+      function(x) {
+        rp <- good_params$reps
+        MMM <- make_miss_mask_mat(MG, x, rp)
+        gp_unlinked <- good_params
+        gp_unlinked$reps <- nrow(MMM)
+        plist1 <- list(
+          C = C,
+          miss_mask_mat = MMM
+        )
+        plist <- c(plist1, gp_unlinked)
+        do.call(simulate_Qij, plist)
+      },
+      mc.cores = num_cores
+    )
+
+    # then we simulate the Qijs linked
+    Qijs_linked <- parallel::mclapply(
+      LTpairs,
+      function(x) {
+        rp <- good_params$reps
+        MMM <- make_miss_mask_mat(MG, x, rp)
+        gp_linked <- good_params
+        gp_linked$reps <- nrow(MMM)
+        plist1 <- list(
+          C = C,
+          miss_mask_mat = MMM,
+          unlinked = FALSE,
+          pedigree_list = pedigrees
+        )
+        plist <- c(plist1, gp_linked)
+        do.call(simulate_Qij, plist)
+      },
+      mc.cores = num_cores
+    )
+
+
+    # then organize those into some list columns
+    Qtib <- tibble(
+      num_non_missing_loci = as.integer(names(Qijs_unlinked)),
+      Qijs_unlinked = Qijs_unlinked,
+      Qijs_linked = Qijs_linked
+    )
+
+
+    ret$Qij <- Qtib %>%
+      mutate(num_missing_loci = L - num_non_missing_loci, .after = num_non_missing_loci)
+
+    return(ret)
+  }
+
+
+
 
 
   #### Look at the range of pairwise non-missing values  ####
