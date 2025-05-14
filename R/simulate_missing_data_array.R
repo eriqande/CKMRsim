dc6fc3cf27db785571d1bbaa606ec51b31c4775f
Eric C. Anderson <eric.anderson@noaa.gov>
2025-05-14 12:51:33 -0600

    Finished up the future_lapply for missing data

--- R/simulate_missing_data_array.R
+++ R/simulate_missing_data_array.R
@@ -1,43 +1,3 @@
-#### Some Helper Functions First ##########
-
-#' Collect Lower Triangle Pairs by Value
-#'
-#' Given a symmetric square integer matrix `M`, this function extracts all (r, c)
-#' index pairs from the lower triangle (r > c) for each unique value `V*` present
-#' in that region of the matrix. The result is a named list where each name corresponds
-#' to a value `V*`, and each element is a list containing two integer vectors: r and c.
-#'
-#' @param M A square numeric matrix. Assumed to be symmetric with integer-like values.
-#'
-#' @return A named list. Each element corresponds to a unique value in the lower triangle of `M`,
-#'         and contains a list with components r and c, the row and column indices (with r > c)
-#'         where that value occurs.
-#'
-#' @examples
-#' set.seed(1)
-#' M <- matrix(sample(80:180, size = 100^2, replace = TRUE), nrow = 100)
-#' result <- collect_lower_triangle_pairs(M)
-#' str(result[["100"]])
-#'
-collect_lower_triangle_pairs <- function(M) {
-  stopifnot(is.matrix(M))
-  stopifnot(nrow(M) == ncol(M))
-
-  n <- nrow(M)
-
-  # Use which() on the lower triangle to get indices
-  lower_indices <- which(lower.tri(M), arr.ind = TRUE)
-  values <- M[lower_indices]
-
-  # Split the row and column indices by the value in the matrix
-  split_indices <- split(as.data.frame(lower_indices), values)
-
-  # Format the result as requested: list named by V*, each element is a list with $r and $c
-  result <- lapply(split_indices, function(df) list(r = df$row, c = df$col))
-
-  return(result)
-}
-
 
 
 #' make a miss_mask_mat for simulate_Qij()
@@ -76,37 +36,30 @@ make_miss_mask_mat <- function(MG, L, reps) {
 
 #' Simulate Qijs across multiple relevant levels of missing data
 #'
+#' This function helps estimate error rates (FPRs/FNRs) under realistic missing data.
 #' Some data sets have a lot of missing markers.  If this is the case, it is not
 #' OK to just do the simulations as if there is no missing data.  This function
 #' wraps up a lot of different steps that can be taken to try to get more accurate
 #' "first-pass" FPRs and FNRs for situations with a lot of missing data.
-#' The steps are:
-#' - Tabulate the distribution of the number of informative (i.e., not missing in
-#'   either member of the pair) markers, across all pairs. (Note, this requires
-#'   that you have an actual data set that you are trying to do relationship
-#'   inference in.)
-#' - Estimate missingness rates per locus, and from that calculate the rate of
-#'   missingness in pairs, under a simple independence assumption.
-#' - Simulate Q_ij values at a series of different numbers of non-missing loci
-#'   to calculate FPRs and FNRs for those.
 #'
-#' Note that this function requires a CKMR object `C` that is suitable for both
-#' linked an unlinked simulation.  So, if you don't have that, then you best
-#' prepare it, even if it means sprinkling your markers into a pseudogenome
-#' as described [here](https://eriqande.github.io/tws-ckmr-2022/kin-finding-lab.html#power-for-kin-finding-while-accounting-for-physical-linkage).
-#' @return This function returns a list.  More on that later.
-#' @inheritParams create_integer_genotype_matrix
+#' Note that this function requires a path to the output of `summarize_missing_data()`.
+#' Rather than passing things in directly, we write an intermediate, uncompressed
+#' file to a temp directory of the necessary inputs so that we can easily parallelize
+#' this on a cluster if needed.
+
+#'
+#' Steps:
+#' 1. Summarize missingness across individuals and pairs
+#' 2. Estimate per-locus missing rates, then derive pairwise missing probabilities.
+#' 3. Record background stats and plots
+#' 4. Clean and prepare dot args
+#' 5. Simulate Qij values using either random missingness (when `simulation_approach == "simple_miss_freq"`)
+#'    or observed pairwise patterns of shared loci (when `simulation_approach == "pairwise_patterns"`).
+#'
+#' @param input_path the absolute path to the file of inputs written out by
+#' `summarize_missing_data()`.
 #' @inheritParams simulate_Qij
-#' @param num_points the number of different values between the lowest observed
-#' number of pairwise non-missing genotypes and the highest, inclusive, that
-#' simulations will be performed for.
-#' @param num_cores Number of cores to parallelize the simulations over (using mclapply)
-#' from the parallel package.  On Windows, parallelization is not available from forking
-#' so this must remain equal to 1 on Windows.
-#' @param tabulate_and_exit if TRUE, then the function exits after tabulating the
-#' number of pairs with different numbers on shared non-missing genotypes, and returns
-#' only information about that.  This is useful for getting an overview of what the
-#' missing data structure in your data look like.
+#' @param num_points Number of points between the observed min and max non-missing loci to simulate.
 #' @param simulation_approach this determines how simulations should be done. The
 #' options are:
 #' - `simple_miss_freq`: do simulations at `num_points` different values of the number
@@ -119,121 +72,93 @@ make_miss_mask_mat <- function(MG, L, reps) {
 #' the observed patterns of missing loci for all pairs. If the number Np of pairs in a category
 #' is less than `reps`, then the actual number of reps done will be the smallest integer
 #' multiple of Np >= `reps`, and the patterns of missing data will be recycled.
+#' @param .parallel_plan Future strategy (e.g., "sequential", "multisession").
+#' @param .workers Number of workers for parallel backend.
+#' @param CKMRsim.discard_stderr logical.  Set to true by default to discard stderr,
+#' @param CKMRsim.discard_stdout logical. Set to true by default to discard stdout
 #' @inheritDotParams simulate_Qij sim_relats calc_relats reps
-#'
+#' @importFrom future plan sequential
+#' @importFrom future.apply future_lapply
 #' @export
-#' @examples
-#' # this is just here for testing at the moment
-#' LG <- read_rds("/tmp/LG.rds")
-#' C <- read_rds("/tmp/C.rds")
 simulate_missing_data_array <- function(
-    LG,
-    C,
+    input_path,
     num_points = 11,
-    num_cores = 1,
     tabulate_and_exit = FALSE,
     simulation_approach = "simple_miss_freq",
-    ...
+    ...,
+    .parallel_plan = "sequential",
+    .workers = NULL,
+    CKMRsim.discard_stderr = TRUE,
+    CKMRsim.discard_stdout = TRUE
 ) {
 
-  ret <- list()
-
-  #### Step 1. Tabulate missing data quantities across pairs  ####
-  AF <- C$orig_data
-  MG <- create_integer_genotype_matrix(LG, AF)
-  L <- ncol(MG)  # number of loci.  Good to have
-
-  # MG is N rows by L columns.  We want to turn values >=0 to 1 and = -1 to 0
-  MG[MG >= 0] <- 1
-  MG[MG == -1] <- 0
-  big <- MG %*% t(MG)
-
-  pairwise_non_miss_counts <- tibble(
-    num_non_miss = big[lower.tri(big)]
-  ) %>%
-    count(num_non_miss)
-
-  pairwise_non_miss_counts_plot <- ggplot(pairwise_non_miss_counts, aes(x = num_non_miss, y = n)) +
-    geom_col() +
-    xlab("Number of loci missing in neither pair member") +
-    ylab("Number of pairs")
-
-  # while we are at it, let's count up the number of non-missing loci in each individual
-  non_miss_counts_by_indiv <- tibble(
-    num_non_miss = rowSums(MG)
-  ) %>%
-    count(num_non_miss)
-
-  non_miss_counts_by_indiv_plot <- ggplot(non_miss_counts_by_indiv, aes(x = num_non_miss, y = n)) +
-    geom_col() +
-    xlab("Number of non-missing loci") +
-    ylab("Number of individuals")
-
-
-  #### Step 1.5*.  Collect the pairs that have the same number of shared loci (if needed)
-  if(simulation_approach == "pairwise_patterns") {
-    LTpairs <- collect_lower_triangle_pairs(big)
-  }
-
-
-  #### Step 2. Estimate missing data rate per locus   ####
-
-  # this is simple
-  miss_rates_by_locus <- colMeans(MG == 0)
-
-  # the rate at which things are non-missing in pairs is
-  pairwise_miss_rates_by_locus = 1 - (1 - miss_rates_by_locus)^2
-
-
-  #### Stop for a moment and start filling up the return list ####
-  ret$background$values$pairwise_non_miss_counts <- pairwise_non_miss_counts
-  ret$background$values$non_miss_counts_by_indiv <- non_miss_counts_by_indiv
-  ret$background$values$miss_rates_by_locus <- miss_rates_by_locus
-  ret$background$values$pairwise_miss_rates_by_locus <- pairwise_miss_rates_by_locus
-
-  ret$background$plots$non_miss_counts_by_indiv_plot <- non_miss_counts_by_indiv_plot
-  ret$background$plots$pairwise_non_miss_counts_plot <- pairwise_non_miss_counts_plot
-
-  # bail out if only tabulating and exiting
-  if(tabulate_and_exit) {
-    return(ret)
-  }
-
-
-
-  #### If we are simulating using pairwise patterns, we can just do it here
-  if(simulation_approach == "pairwise_patterns") {
-    # deal with ... stuff
-    dotL <- list(...)
-    bad_params_logi <- !(names(dotL) %in% c("sim_relats", "calc_relats", "reps"))
-    if(sum(bad_params_logi) > 1) {
-      bad_params <- names(dotL)[bad_params_logi]
-      message("Ignoring ... params: ", paste(bad_params, collapse = ", "))
+  # Set up future parallel plan
+  if (is.character(.parallel_plan)) {
+    if (.parallel_plan == "sequential") {
+      future::plan(future::sequential)
+    } else if (.parallel_plan == "multisession") {
+      future::plan(future::multisession, workers = .workers %||% num_cores)
+    } else if (.parallel_plan == "multicore") {
+      future::plan(future::multicore, workers = .workers %||% num_cores)
+    } else {
+      stop("Unknown .parallel_plan string: ", .parallel_plan)
     }
-    good_params <- dotL[!bad_params_logi]
+  } else {
+    future::plan(.parallel_plan)
+  }
 
-    # first, we simulate the Qijs unlinked
-    Qijs_unlinked <- parallel::mclapply(
-      LTpairs,  #### Just do first 20 for testing!
+
+  ####  clean and prepare dot args ####
+  dotL <- list(...)
+  bad_params_logi <- !(names(dotL) %in% c("sim_relats", "calc_relats", "reps"))
+  if (sum(bad_params_logi) > 1) {
+    message("Ignoring ... params: ", paste(names(dotL)[bad_params_logi], collapse = ", "))
+  }
+  good_params <- dotL[!bad_params_logi]
+
+
+  #### Get the lower-triangle pairs list needed to know what to future_lapply() over
+  inList <- read_rds(input_path)
+  LTpairs <- inList$LTpairs
+  L <- ncol(inList$IG)  # total number of loci
+  pairwise_non_miss_counts <- inList$pairwise_non_miss_counts
+  ret <- list()  # will return things in a list (more flexible, if we want to add return items later)
+  rm(inList)
+
+
+  #### Step 3a: simulate using observed pairwise missing patterns ####
+  if (simulation_approach == "pairwise_patterns") {
+    Qijs_unlinked <- future.apply::future_lapply(
+      LTpairs,
       function(x) {
+        options(
+          CKMRsim.discard_stderr = TRUE,
+          CKMRsim.discard_stdout = TRUE
+        )
+        Inputs <- read_rds(input_path)
+        MG <- Inputs$IG
+        C <- Inputs$C
         rp <- good_params$reps
         MMM <- make_miss_mask_mat(MG, x, rp)
         gp_unlinked <- good_params
         gp_unlinked$reps <- nrow(MMM)
-        plist1 <- list(
-          C = C,
-          miss_mask_mat = MMM
-        )
+        plist1 <- list(C = C, miss_mask_mat = MMM)
         plist <- c(plist1, gp_unlinked)
         do.call(simulate_Qij, plist)
       },
-      mc.cores = num_cores
+      future.seed = TRUE
     )
 
-    # then we simulate the Qijs linked
-    Qijs_linked <- parallel::mclapply(
+    Qijs_linked <- future.apply::future_lapply(
       LTpairs,
       function(x) {
+        options(
+          CKMRsim.discard_stderr = TRUE,
+          CKMRsim.discard_stdout = TRUE
+        )
+        Inputs <- read_rds(input_path)
+        MG <- Inputs$IG
+        C <- Inputs$C
         rp <- good_params$reps
         MMM <- make_miss_mask_mat(MG, x, rp)
         gp_linked <- good_params
@@ -247,54 +172,43 @@ simulate_missing_data_array <- function(
         plist <- c(plist1, gp_linked)
         do.call(simulate_Qij, plist)
       },
-      mc.cores = num_cores
+      future.seed = TRUE
     )
 
-
-    # then organize those into some list columns
     Qtib <- tibble(
       num_non_missing_loci = as.integer(names(Qijs_unlinked)),
       Qijs_unlinked = Qijs_unlinked,
       Qijs_linked = Qijs_linked
     )
 
-
     ret$Qij <- Qtib %>%
       mutate(num_missing_loci = L - num_non_missing_loci, .after = num_non_missing_loci)
+
+    # Reset plan (recommended in packages)
+    future::plan(future::sequential)
 
     return(ret)
   }
 
-
-
-
-
-  #### Look at the range of pairwise non-missing values  ####
+  #### Step 2b: simulate using random missingness frequencies ####
   rg <- range(pairwise_non_miss_counts$num_non_miss)
   span <- rg[2] - rg[1]
   step <- span / (num_points - 1)
   sim_L_vals <- round(seq(from = rg[1], to = rg[2], by = step))
-
-  # get that as the number of missing loci too
   sim_Miss_vals <- L - sim_L_vals
   names(sim_Miss_vals) <- sim_Miss_vals
 
-
-  #### Run simulations at the multiple sim_Miss_vals, in parallel ####
-  if(simulation_approach == "simple_miss_freq") {
-    # deal with ... stuff
-    dotL <- list(...)
-    bad_params_logi <- !(names(dotL) %in% c("sim_relats", "calc_relats", "reps"))
-    if(sum(bad_params_logi) > 1) {
-      bad_params <- names(dotL)[bad_params_logi]
-      message("Ignoring ... params: ", paste(bad_params, collapse = ", "))
-    }
-    good_params <- dotL[!bad_params_logi]
-
-    # first, we simulate the Qijs unlinked
-    Qijs_unlinked <- parallel::mclapply(
+  if (simulation_approach == "simple_miss_freq") {
+    Qijs_unlinked <- future.apply::future_lapply(
       sim_Miss_vals,
       function(x) {
+        options(
+          CKMRsim.discard_stderr = TRUE,
+          CKMRsim.discard_stdout = TRUE
+        )
+        Inputs <- read_rds(input_path)
+        C <- Inputs$C
+        pairwise_miss_rates_by_locus = Inputs$pairwise_miss_rates_by_locus
         plist1 <- list(
           C = C,
           rando_miss_wts = pairwise_miss_rates_by_locus,
@@ -303,13 +217,19 @@ simulate_missing_data_array <- function(
         plist <- c(plist1, good_params)
         do.call(simulate_Qij, plist)
       },
-      mc.cores = num_cores
+      future.seed = TRUE
     )
 
-    # then we simulate the Qijs linked
-    Qijs_linked <- parallel::mclapply(
+    Qijs_linked <- future.apply::future_lapply(
       sim_Miss_vals,
       function(x) {
+        options(
+          CKMRsim.discard_stderr = TRUE,
+          CKMRsim.discard_stdout = TRUE
+        )
+        Inputs <- read_rds(input_path)
+        C <- Inputs$C
+        pairwise_miss_rates_by_locus = Inputs$pairwise_miss_rates_by_locus
         plist1 <- list(
           C = C,
           rando_miss_wts = pairwise_miss_rates_by_locus,
@@ -320,24 +240,25 @@ simulate_missing_data_array <- function(
         plist <- c(plist1, good_params)
         do.call(simulate_Qij, plist)
       },
-      mc.cores = num_cores
+      future.seed = TRUE
     )
 
-
-    # then organize those into some list columns
     Qtib <- tibble(
       num_missing_loci = as.integer(names(Qijs_unlinked)),
       Qijs_unlinked = Qijs_unlinked,
       Qijs_linked = Qijs_linked
     )
 
-
     ret$Qij <- Qtib %>%
       mutate(num_non_missing_loci = L - num_missing_loci, .before = num_missing_loci)
 
-    return(ret)
+    # Reset plan (recommended in packages)
+    future::plan(future::sequential)
 
+    return(ret)
   } else {
     stop("Not a recognized simulation approach: ", simulation_approach)
   }
+
+
 }
---
+++ R/summarize_missing_data.R
@@ -0,0 +1,196 @@
+#### Some Helper Functions First ##########
+
+#' Collect Lower Triangle Pairs by Value
+#'
+#' Given a symmetric square integer matrix `M`, this function extracts all (r, c)
+#' index pairs from the lower triangle (r > c) for each unique value `V*` present
+#' in that region of the matrix. The result is a named list where each name corresponds
+#' to a value `V*`, and each element is a list containing two integer vectors: r and c.
+#'
+#' @param M A square numeric matrix. Assumed to be symmetric with integer-like values.
+#'
+#' @return A named list. Each element corresponds to a unique value in the lower triangle of `M`,
+#'         and contains a list with components r and c, the row and column indices (with r > c)
+#'         where that value occurs.
+#'
+#' @examples
+#' set.seed(1)
+#' M <- matrix(sample(80:180, size = 100^2, replace = TRUE), nrow = 100)
+#' result <- collect_lower_triangle_pairs(M)
+#' str(result[["100"]])
+#'
+collect_lower_triangle_pairs <- function(M) {
+  stopifnot(is.matrix(M))
+  stopifnot(nrow(M) == ncol(M))
+
+  n <- nrow(M)
+
+  # Use which() on the lower triangle to get indices
+  lower_indices <- which(lower.tri(M), arr.ind = TRUE)
+  values <- M[lower_indices]
+
+  # Split the row and column indices by the value in the matrix
+  split_indices <- split(as.data.frame(lower_indices), values)
+
+  # Format the result as requested: list named by V*, each element is a list with $r and $c
+  result <- lapply(split_indices, function(df) list(r = df$row, c = df$col))
+
+  return(result)
+}
+
+
+
+
+#' Write a large R object to a temp file safely for SLURM/multinode environments
+#'
+#' Writes an object to a uniquely named .rds file, preferring a known shared directory
+#' if available. Warns if writing to a possibly node-local TMPDIR in cluster environments.
+#'
+#' @param object The R object to write.
+#' @param prefix Optional prefix for the filename.
+#' @param compress Logical. Use RDS compression? Default is FALSE for speed.
+#' @param dir Directory to write the temp file to. Defaults to CKMR_SHARED_TMPDIR,
+#'            then TMPDIR, then R's tempdir().
+#' @param warn_if_local Logical. Warn if writing to possibly node-local TMPDIR. Default TRUE.
+#' @return The full path to the saved .rds file.
+write_large_temp_object <- function(object,
+                                    prefix = "ckmr-",
+                                    compress = FALSE,
+                                    dir = NULL,
+                                    warn_if_local = TRUE) {
+  # Resolve target directory in priority order
+  target_dir <- dir %||%
+    Sys.getenv("CKMRSIM_SHARED_TMPDIR", unset = Sys.getenv("TMPDIR", unset = tempdir()))
+
+  # Warn if $TMPDIR is likely to be node-local
+  if (warn_if_local &&
+      identical(target_dir, Sys.getenv("TMPDIR")) &&
+      !grepl("/scratch|/work|/projects", target_dir)) {
+    warning("Writing to TMPDIR (", target_dir, "), which may be node-local and not accessible to other SLURM nodes.\n",
+            "Consider setting CKMRSIM_SHARED_TMPDIR to a shared directory.")
+  }
+
+  if (!dir.exists(target_dir)) {
+    message("Creating target temp directory that did not previously exist: ", target_dir)
+    dir.create(target_dir, recursive = TRUE)
+  }
+
+  tmpfile <- tempfile(pattern = prefix, tmpdir = target_dir, fileext = ".rds")
+  saveRDS(object, file = tmpfile, compress = compress)
+
+  return(tmpfile)
+}
+
+
+
+######## END OF HELPER FUNCTIONS #################
+
+
+
+#' Summarize the missing data and make some plots.
+#'
+#' In default mode, this will just summarize the missing data and make and
+#' store some plots in the list output.  If you provide a temp directory
+#' path for `downstream_tempdir`, then this function will assemble all that
+#' is needed to run `simulate_missing_data_array()` and will write that out
+#' to a tempfile in that temp directory so that it can be accessed by different
+#' workers from within a `future_lapply()` call so that this can be parallelized
+#' across a cluster.
+#'
+#' Note that C must be a CKMR object that is suitable for *both* linked and unlinked
+#' simulation.  So, if you don't have that, then you best
+#' prepare it, even if it means sprinkling your markers into a pseudogenome
+#' as described [here](https://eriqande.github.io/tws-ckmr-2022/kin-finding-lab.html#power-for-kin-finding-while-accounting-for-physical-linkage).
+#'
+#' @return A list containing summary plots, statistics, and simulation results, or
+#' the absolute path to a tempfile with inputs needed for `simulate_missing_data_array()`.
+#' @inheritParams create_integer_genotype_matrix
+#' @param downstream_tempdir By default, this is NA.  If not NA, then the function will
+#' write the necessary objects for `simulate_missing_data_array()` into a temp file
+#' within this temp dir, and it will return the path to that file (to be passed into
+#' `simulate_missing_data_array()`).  If you are working in a cluster environment on
+#' different nodes, make sure that `downstream_tempdir` points to storage that is
+#' accessible from all nodes (for example, a directory in scratch, etc.). If you set
+#' downstream_tempdir to NULL, then the function will try to use the environment variables
+#' CKMRSIM_SHARED_TMPDIR and TMPDIR, in that order, before just using a standard
+#' R temp directory
+#' @export
+summarize_missing_data <- function(
+    LG,
+    C,
+    downstream_tempdir = NA
+) {
+
+  ret <- list()
+
+  #### Step 1: summarize missingness across individuals and pairs ####
+  AF <- C$orig_data
+  MG <- create_integer_genotype_matrix(LG, AF)
+  L <- ncol(MG)  # number of loci
+
+  MG[MG >= 0] <- 1  # recode genotypes as 1 (non-missing)
+  MG[MG == -1] <- 0 # missing values become 0
+  big <- MG %*% t(MG)
+
+  pairwise_non_miss_counts <- tibble(
+    num_non_miss = big[lower.tri(big)]
+  ) %>%
+    count(num_non_miss)
+
+  pairwise_non_miss_counts_plot <- ggplot(pairwise_non_miss_counts, aes(x = num_non_miss, y = n)) +
+    geom_col() +
+    xlab("Number of loci missing in neither pair member") +
+    ylab("Number of pairs")
+
+  non_miss_counts_by_indiv <- tibble(
+    num_non_miss = rowSums(MG)
+  ) %>%
+    count(num_non_miss)
+
+  non_miss_counts_by_indiv_plot <- ggplot(non_miss_counts_by_indiv, aes(x = num_non_miss, y = n)) +
+    geom_col() +
+    xlab("Number of non-missing loci") +
+    ylab("Number of individuals")
+
+  # Collect the lower triangle regardless of which simulation approach is being used
+  # because we want to return it if tabulate and exit is true
+  LTpairs <- collect_lower_triangle_pairs(big)
+  rm(big)
+
+  #### Step 2: estimate missingness rates ####
+  miss_rates_by_locus <- colMeans(MG == 0)
+  pairwise_miss_rates_by_locus <- 1 - (1 - miss_rates_by_locus)^2
+
+  #### Step 3: record background stats and plots ####
+  ret$background$values$pairwise_non_miss_counts <- pairwise_non_miss_counts
+  ret$background$values$non_miss_counts_by_indiv <- non_miss_counts_by_indiv
+  ret$background$values$miss_rates_by_locus <- miss_rates_by_locus
+  ret$background$values$pairwise_miss_rates_by_locus <- pairwise_miss_rates_by_locus
+  ret$background$values$lower_triangle_pairs <- LTpairs
+  ret$background$values$indiv_names_tibble <- tibble::tibble(Indiv = rownames(MG)) %>%
+    mutate(idx = 1:n(), .before = Indiv)
+
+  ret$background$plots$non_miss_counts_by_indiv_plot <- non_miss_counts_by_indiv_plot
+  ret$background$plots$pairwise_non_miss_counts_plot <- pairwise_non_miss_counts_plot
+
+  if (is.na(downstream_tempdir)) return(ret)
+
+  # if downstream_tempdir is not NA, then we compile up the needed inputs
+  # for simulate_missing_data_array.
+  tosave <- list(
+    C = C,
+    IG = MG,   # these are the integer-represented genotypes in a matrix
+    LTpairs = LTpairs,
+    indiv_names_tibble = ret$background$values$indiv_names_tibble,
+    pairwise_miss_rates_by_locus = pairwise_miss_rates_by_locus,
+    pairwise_non_miss_counts = pairwise_non_miss_counts
+  )
+
+  path <- write_large_temp_object(
+    tosave,
+    dir = downstream_tempdir
+  )
+
+  normalizePath(path)
+
+}
