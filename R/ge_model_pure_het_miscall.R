6fda36cc2e0fa4f04787303e3e64361b3ced2e16
Eric C. Anderson <eric.anderson@noaa.gov>
2025-06-25 21:03:33 -0700

    Add smidge to the pure_het_miscall

--- R/ge_model_pure_het_miscall.R
+++ R/ge_model_pure_het_miscall.R
@@ -1,19 +1,26 @@
-#' implements a simple true-genotype-independent genotyping error model
+#' implements a simple heterozygote miscall model with a modification for Mendelian incompatibilities
 #'
-#' Note that this is sort of silly in the context of parentage inference
+#' Note that the pure heterozygote miscall model is a little bit silly because
+#' alternate homozygotes have no chance of carrying another allele which leads to
+#' some issues with in the context of parentage inference
 #' because for non-parentals, it can lead to probability of zero for
-#' Mendelian incompatibilities.  Probably better to use the ge_model_microhap1().
+#' Mendelian incompatibilities.  So, we start off with a pure heterozygote
+#' miscall model, and then we replace zeroes in each row with smidge divided
+#' by the number of zeroes.  We basically just don't want to have any zeros
+#' because that could lead to probabilities of zero.
 #'
 #' @param L required locus specific information
-#' @param epsilon the rate at which genotypes are incorrectly observed.
+#' @param m the heterozygote miscall rate
+#' @param smidge the total amount added to zeroes in a row.
 #' @export
 #' @examples
 #' L <- list(
 #'  freqs = c(A = 0.6, B = 0.4),
 #'  geno_freqs = c(`A / A` = 0.36, `A / B` = 0.48, `B / B` = 0.16)
 #' )
-#' ge_model_TGIE(L, m = 0.185)
-ge_model_pure_het_miscall <- function(L, m) {
+#' ge_model_pure_het_miscall(L, m = 0.0285, smidge = 0.001)
+ge_model_pure_het_miscall <- function(L, m, smidge) {
+
   # first make a matrix G x G matrix of the genotype frequencies
   gmat <- matrix(rep(L$geno_freqs, length(L$geno_freqs)), byrow = TRUE, nrow = length(L$geno_freqs))
 
@@ -41,12 +48,24 @@ ge_model_pure_het_miscall <- function(L, m) {
   # that combines allelic mis-calls and dropouts. Note that by setting Ws
   # to W here we assume that the occurrence of a dropout does not affect the
   # rate at which mis-calls occur.
-  ret <- combine_miscalls_and_dropouts(
+  hetm <- combine_miscalls_and_dropouts(
     geno_names = names(L$geno_freqs),
     D = D,
     W = W,
     Ws = W
   )
+
+  ret <- apply(
+    hetm,
+    1,
+    function(x) {
+      numz <- sum(x == 0)
+      if(numz > 0) {
+        x[x == 0] <- smidge / numz
+      }
+      x / sum(x)
+    }) %>%
+    t()
 
   ret
 }
